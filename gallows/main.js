5f854e8586ffdeb3f6ab77d6098155c962062072
gremliiin <rekufo@gmail.com>
2020-03-12 12:44:46 +0600

    My works for learn

--- gallows/main.js
+++ gallows/main.js
@@ -26,7 +26,7 @@ let checkWin = true;
 while (remainingLetters > 0 || checkUnderscore === true) {
     alert(answerArray.join(" "));
     let guess = prompt("Угадайте букву или нажмите 'Отмена' для выхода\n У вас есть 15 попыток ", "");
-        
+
     if (guess === null) {
         alert("Вы вышли из игры");
         checkWin = false;
@@ -34,15 +34,15 @@ while (remainingLetters > 0 || checkUnderscore === true) {
     } else if (guess.length !== 1) {
         alert("Пожалуйста, введите только одну букву");
     } else {
-        for (let j = 0; j < word.length; j++) {
-            if (word[j] === guess.toLowerCase()) {
-                answerArray[j] = guess.toLowerCase();
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] === guess.toLowerCase()) {
+                answerArray[i] = guess.toLowerCase();
                 remainingLetters--;
             }
-            
+
         }
-        for (let k = 0; k < word.length; k++){
-            if(answerArray[k] === "_"){
+        for (let j = 0; j < word.length; j++) {
+            if (answerArray[j] === "_") {
                 checkUnderscore = true;
                 break;
             } else {
@@ -51,18 +51,17 @@ while (remainingLetters > 0 || checkUnderscore === true) {
         }
 
         countTry--;
-        if(countTry === 0){
+        if (countTry === 0) {
             alert("У вас закончились попытки! Вы проиграли");
             checkWin = false;
             break;
         }
-        console.log(word);
     }
 }
 
-if(checkWin === true){
+if (checkWin === true) {
     alert("Вы выйграли: " + answerArray.join(" "));
     alert("Было загадано слово " + word);
-} else if (checkWin === false){
+} else if (checkWin === false) {
     alert("Было загадано слово " + word);
 }
--- project_two/main.js
+++ project_two/main.js
@@ -1,2 +1,23 @@
-// 1) + 2) + 3) + 4) -  5) - 6) - 7) - 8) + 9) - 10) + 11) + 12) -
+let Car= function(x, y){
+    this.x = x;
+    this.y = y;
+};
 
+let DrawCar = function (car) {
+    let carHtml = '<img src="http://nostarch.com/images/car.png">';
+    let carElement = $(carHtml);
+
+    carElement.css({
+        position: "absolute",
+        left: car.x,
+        top: car.y,
+    });
+
+    $("body").append(carElement);
+};
+
+let Nissan = new Car(20, 20);
+let Tesla = new Car(100, 200);
+
+DrawCar(Nissan);
+DrawCar(Tesla);
